cceabf602779e2530c6ab93285c4be8625ba4e16
Rubylenshy <reztomoloju@gmail.com>
2023-01-12 04:48:41 +0100

    mobile-footer script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,16 @@
 
-const sideBarBtn = document.querySelectorAll('.step-circle')
 
-for (let i = 0; i < sideBarBtn.length; i++) {
-    var stepOne = document.querySelector('.step-information-one');
-    var stepTwo = document.querySelector('.step-information-two');
-    var stepThree = document.querySelector('.step-information-three');
-    var stepFour = document.querySelector('.step-information-four');
-    var stepFive = document.querySelector('.step-information-five');
-    window.addEventListener('load', ()=>{
-        sideBarBtn[0].classList.add('active')
-    })
-    
-    sideBarBtn[0].addEventListener('click', ()=>{
-        stepOne.style.display = 'block'
-    })
-    sideBarBtn[1].addEventListener('click', ()=>{
-        stepTwo.style.display = 'block'
-        stepOne.style.display = 'none'
-        stepThree.style.display = 'none'
-    })
-    sideBarBtn[2].addEventListener('click', ()=>{
-        stepThree.style.display = 'block'
-        stepOne.style.display = 'none'
-        stepTwo.style.display = 'none'
-    })
-    sideBarBtn[3].addEventListener('click', ()=>{
-        stepFour.style.display = 'block'
-        stepOne.style.display = 'none'
-        stepTwo.style.display = 'none'
-        stepThree.style.display = 'none'
-    })
-}
+var stepOne = document.querySelector('.step-information-one');
+var stepTwo = document.querySelector('.step-information-two');
+var stepThree = document.querySelector('.step-information-three');
+var stepFour = document.querySelector('.step-information-four');
+var stepFive = document.querySelector('.step-information-five');
+/* Mobile-buttons */
+const mobileFooter = document.querySelector('.mobile-footer')
+const prev = mobileFooter.querySelector('.go-back')
+const next = mobileFooter.querySelector('.next-step-btn')
 
+/*Step Two Switch */
 const switchPlan = document.querySelector('.switch')
 const price = document.querySelectorAll('.bill-price')
 const addOnBill = stepThree.querySelectorAll('.add-on-bill')
@@ -68,18 +47,3 @@ switchPlan.addEventListener('click', ()=>{
         }
 })
 
-const addOns = document.querySelectorAll('.add-ons')
-
-// const divSwitch = document.querySelector('.switch div')
-// const addOnBill = stepThree.querySelectorAll('.add-on-bill')
-// if (divSwitch.style.justifyContent = 'flex-end') {
-//     for (let i = 0; i < addOnBill.length; i++) {
-//         addOnBill[0].textContent = '+$10/yr'
-//         addOnBill[1].textContent = '+$20/yr'
-//         addOnBill[2].textContent = '+$20/yr'
-//     }
-// }else{
-//     addOnBill[0].textContent = '+$1/mo'
-//     addOnBill[1].textContent = '+$2/mo'
-//     addOnBill[2].textContent = '+$2/mo'
-// }
